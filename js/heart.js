0d018012b3c99fad79901e193ee6d46dc2695fd6
barrajanos <59731227+barrajanos@users.noreply.github.com>
2021-01-13 17:26:08 +0100

    cc
     ccc

--- js/heart.js
+++ js/heart.js
@@ -30210,6 +30210,7 @@
                   this.loader.load(
                     "rocket.glb",
                     function (t) {
+                        new i.Box3().setFromObject(t.scene);
                       t.scene.traverse(function (t) {
                         t.isMesh,
                           "Voronoi_Fracture" === t.name &&
