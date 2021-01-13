bf0b071880283dd6dac60a2f069ad13907b76047
barrajanos <59731227+barrajanos@users.noreply.github.com>
2021-01-13 14:59:18 +0100

    Update heart.js
    dsd

--- js/heart.js
+++ js/heart.js
@@ -30180,7 +30180,7 @@
               0.001,
               1e3
             )),
-            this.camera.position.set(0, 0, 7),
+            this.camera.position.set(0, 0, 250),
             (this.time = 0),
             (this.loader = new i.GLTFLoader().setPath("models/")),
             i.DRACOLoader.setDecoderPath("js/lib/draco/"),
