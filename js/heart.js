94cf81e86f9d74f80581af332c40834b18bdb770
barrajanos <59731227+barrajanos@users.noreply.github.com>
2021-01-13 20:23:26 +0100

    xycyxc
    cxy

--- js/heart.js
+++ js/heart.js
@@ -30208,9 +30208,9 @@
                 new i.Vector3(0, 0, 0);
                 (this.voron = []),
                   this.loader.load(
-                    "rocket.drc",
+                    "heart-high.glb",
                     function (t) {
-                        new i.Box3().setFromObject(t.scene);
+
                       t.scene.traverse(function (t) {
                         t.isMesh,
                           "Voronoi_Fracture" === t.name &&
